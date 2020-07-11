50827c58f297b223f43d061e4f293730dee99612
Luke Rodgers <lukeasrodgers@gmail.com>
2020-07-11 12:07:42 -0400

    iniital commit

---
+++ ch1/java/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer n = 1;
+	    System.out.println(n);
+	    n += 1;
+        System.out.println(n);
+        Integer integer = n++;
+        System.out.println(n);
+    }
+}
