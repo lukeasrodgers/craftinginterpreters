59d0fda9e10005e90f2f456b9e8c25b14d2e9d33
Luke Rodgers <lukeasrodgers@gmail.com>
2022-05-14 21:01:27 -0400

    10.2 native functions

--- lox/src/com/craftinginterpreters/lox/Interpreter.java
+++ lox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -8,8 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Object> {
+    final Environment globals = new Environment();
+    private Environment environment = globals;
 
-    private Environment environment = new Environment();
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() { return 0; }
+
+            @Override
+            public Object call(Interpreter interpreter, List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() { return "<native fn>"; }
+        });
+    }
 
     void interpret(List<Stmt> statements) {
         try {
