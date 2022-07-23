11c53285f8d54220ea1f294c9c74eb50a83b4723
Luke Rodgers <lukeasrodgers@gmail.com>
2022-07-23 15:42:57 -0400

    AST generation for this

--- lox/src/com/craftinginterpreters/lox/Expr.java
+++ lox/src/com/craftinginterpreters/lox/Expr.java
@@ -12,6 +12,7 @@ abstract class Expr {
     R visitLiteralExpr(Literal expr);
     R visitLogicalExpr(Logical expr);
     R visitSetExpr(Set expr);
+    R visitThisExpr(This expr);
     R visitUnaryExpr(Unary expr);
     R visitVariableExpr(Variable expr);
   }
@@ -62,7 +63,7 @@ abstract class Expr {
     final List<Expr> arguments;
   }
   static class Get extends Expr {
-    Get(Expr object, Token name) {
+    Get(Expr object, Token name) { 
       this.object = object;
       this.name = name;
     }
@@ -130,6 +131,18 @@ abstract class Expr {
     final Expr object;
     final Token name;
     final Expr value;
+  }
+  static class This extends Expr {
+    This(Token keyword) { 
+      this.keyword = keyword;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitThisExpr(this);
+    }
+
+    final Token keyword;
   }
   static class Unary extends Expr {
     Unary(Token operator, Expr right) { 
