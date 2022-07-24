5b49262e55d4f678177c6c8e9e39ffb41ccfe381
Luke Rodgers <lukeasrodgers@gmail.com>
2022-07-24 11:11:46 -0400

    12.6 Finish "this" with test for invalid usage at parse time

--- lox/src/com/craftinginterpreters/lox/Resolver.java
+++ lox/src/com/craftinginterpreters/lox/Resolver.java
@@ -9,6 +9,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private final Interpreter interpreter;
     private final Stack<Map<String, Boolean>> scopes = new Stack<>();
     private FunctionType currentFunction = FunctionType.NONE;
+    private enum ClassType {
+        NONE,
+        CLASS
+    }
+    private ClassType currentClass = ClassType.NONE;
 
     Resolver(Interpreter interpreter) {
         this.interpreter = interpreter;
@@ -30,6 +35,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitClassStmt(Stmt.Class stmt) {
+        ClassType enclosingClass = currentClass;
+        currentClass = ClassType.CLASS;
         declare(stmt.name);
         define(stmt.name);
 
@@ -42,6 +49,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         endScope();
+        currentClass = enclosingClass;
         return null;
     }
 
@@ -133,6 +141,10 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitThisExpr(Expr.This expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Can't use 'this' oustide of a class.");
+            return null;
+        }
         resolveLocal(expr, expr.keyword);
         return null;
     }
