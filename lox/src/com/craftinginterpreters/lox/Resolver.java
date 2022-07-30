bbdd89b30874067665f381b4142e7a723f7dbfb8
Luke Rodgers <lukeasrodgers@gmail.com>
2022-07-30 11:15:21 -0400

    13.3.3 invalid uses of super

--- lox/src/com/craftinginterpreters/lox/Resolver.java
+++ lox/src/com/craftinginterpreters/lox/Resolver.java
@@ -11,7 +11,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private FunctionType currentFunction = FunctionType.NONE;
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
     private ClassType currentClass = ClassType.NONE;
 
@@ -46,6 +47,7 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
         }
 
@@ -164,6 +166,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Can't use 'super' oustide of a class.");
+        } else if (currentClass == ClassType.CLASS) {
+            Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+        }
         resolveLocal(expr, expr.keyword);
         return null;
     }
