79d50f12abd35f89165527180eec1367e9b86b68
Luke Rodgers <lukeasrodgers@gmail.com>
2022-07-30 11:10:33 -0400

    13.3.2 super semantics

--- lox/src/com/craftinginterpreters/lox/Interpreter.java
+++ lox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -80,6 +80,18 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Object> {
     }
 
     @Override
+    public Object visitSuperExpr(Expr.Super expr) {
+        int distance = locals.get(expr);
+        LoxClass superclass = (LoxClass)environment.getAt(distance, "super");
+        LoxInstance object = (LoxInstance)environment.getAt(distance - 1, "this");
+        LoxFunction method = superclass.findMethod(expr.method.lexeme);
+        if (method == null) {
+            throw new RuntimeError(expr.method, "Undfined property '" + expr.method.lexeme + "'.");
+        }
+        return method.bind(object);
+    }
+
+    @Override
     public Object visitThisExpr(Expr.This expr) {
         return lookupVariable(expr.keyword, expr);
     }
@@ -131,6 +143,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Object> {
         }
         environment.define(stmt.name.lexeme, null);
 
+        if (stmt.superclass != null) {
+            environment = new Environment(environment);
+            environment.define("super", superclass);
+        }
+
         Map<String, LoxFunction> methods = new HashMap<>();
         for (Stmt.Function method : stmt.methods) {
             LoxFunction function = new LoxFunction(method, environment, method.name.lexeme == "init");
@@ -138,6 +155,9 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Object> {
         }
 
         LoxClass klass = new LoxClass(stmt.name.lexeme, (LoxClass)superclass, methods);
+        if (superclass !=  null) {
+            environment = environment.enclosing;
+        }
         environment.assign(stmt.name, klass);
         return null;
     }
