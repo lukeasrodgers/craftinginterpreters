c74f032dfd111c3b98a82dddf33d4a49b94288cb
Luke Rodgers <lukeasrodgers@gmail.com>
2022-09-18 09:21:28 -0400

    basic stack operations for clox + makefile

--- clox/main.c
+++ clox/main.c
@@ -1,8 +1,10 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char* argv[]) {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
@@ -10,15 +12,13 @@ int main(int argc, char* argv[]) {
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_CONSTANT, 124);
-    int constant2 = addConstant(&chunk, 1.4);
-    writeChunk(&chunk, constant2, 124);
-    writeChunk(&chunk, OP_CONSTANT, 124);
-    writeChunk(&chunk, constant2, 124);
+    writeChunk(&chunk, OP_NEGATE, 123);
 
-    writeChunk(&chunk, OP_RETURN, 125);
+    writeChunk(&chunk, OP_RETURN, 124);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
