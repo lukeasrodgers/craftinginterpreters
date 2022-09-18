9d0e4c16821ee793ad4f3950a06f8b444dd55500
Luke Rodgers <lukeasrodgers@gmail.com>
2022-09-18 09:36:38 -0400

    15.3 clox arithmetic calculator

--- clox/vm.c
+++ clox/vm.c
@@ -37,6 +37,12 @@ InterpretResult interpret(Chunk* chunk) {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
 
 
     for (;;) {
@@ -57,6 +63,10 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:     BINARY_OP(+); break;
+            case OP_SUBTRACT:     BINARY_OP(-); break;
+            case OP_MULTIPLY:     BINARY_OP(*); break;
+            case OP_DIVIDE:     BINARY_OP(/); break;
             case OP_NEGATE: push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
@@ -67,4 +77,5 @@ static InterpretResult run() {
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
