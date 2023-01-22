55d6eb2f7a2bba90fff171018e92c6bdf12e044e
Luke Rodgers <lukeasrodgers@gmail.com>
2023-01-22 15:34:45 -0500

    25.4 closed upvalues

--- clox/debug.c
+++ clox/debug.c
@@ -69,6 +69,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
                 printf("%04d      |                     %s %d\n", offset - 2, isLocal ? "local" : "upvalue", index);
             }
             return offset;
+        case OP_CLOSE_UPVALUE:
+            return simpleInstruction("OP_CLOSE_UPVALUE", offset);
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
         case OP_NEGATE:
